4a7d16ce59b37bb1a645668b084595d0a608b828
Joshua <dbtjq7272@naver.com>
2019-07-23 14:00:34 +0900

    1st test

---
+++ Desktop/Develop/_software_G2G/Android_GS5001/MakingJAR/app/src/main/java/com/g2g/makingjar/Calculate.java
@@ -0,0 +1,18 @@
+package com.g2g.makingjar;
+
+public class Calculate {
+    int a;
+    int b;
+    public Calculate (){
+        a = 100;
+        b = 200;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+}
